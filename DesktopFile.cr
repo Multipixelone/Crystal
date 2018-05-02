85399e8f8995ca4a0bfd9aba359f7f75a8c15399
Multipixelone <5051116+Multipixelone@users.noreply.github.com>
2018-05-01 22:59:16 -0700

    Add desktop file, add first few lines

---
+++ DesktopFile.cr
@@ -0,0 +1,17 @@
+# Generate a Linux .desktop file
+puts ".Desktop file script made by Multipixelone"
+
+# Ask some questions
+puts "Type a simple name of the application in lowercase, with no spaces, followed by [ENTER]:"
+simple = gets
+echo "Type the full name of the application, followed by [ENTER]:"
+fullname = gets
+echo "Command to execute, followed by [ENTER]:"
+command = gets
+echo "Type the comment of the application, followed by [ENTER]:"
+comment = gets
+
+puts simple
+puts fullname
+puts command
+puts comment
