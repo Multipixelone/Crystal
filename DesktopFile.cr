f7e86827c25213aa92f9162ff5913800385eeb27
Multipixelone <5051116+Multipixelone@users.noreply.github.com>
2018-05-02 11:31:25 -0700

    Add default location

--- DesktopFile.cr
+++ DesktopFile.cr
@@ -1,4 +1,7 @@
 # Generate a Linux .desktop file
+# Define default variables
+folder = "DesktopFiles"
+
 puts ".Desktop file script made by Multipixelone"
 
 # Ask some questions
@@ -12,13 +15,15 @@ puts "Type the comment of the application, followed by [ENTER]:"
 comment = gets
 
 filename = "#{simple}.desktop"
+filepath = "#{folder}/#{filename}"
 
-puts "\e[H\e[2J"
+print "\e[H\e[2J"
 puts "Please make sure these are the right variables:"
 puts "Name of desktop file: #{filename}"
 puts "Name of Program in list: #{fullname}"
 puts "Command to execute: #{command}"
 puts "Comment in list: #{comment}"
+puts "Location of desktop file: #{filepath}"
 gets
 
 Dir.mkdir("DesktopFiles")
