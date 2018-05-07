348f1902e2408968d31c57f9b1834a6b3fc9b5da
Multipixelone <5051116+Multipixelone@users.noreply.github.com>
2018-05-06 23:06:27 -0700

    Change to Cm/s

--- src/GravityCalc.cr
+++ src/GravityCalc.cr
@@ -1,15 +1,24 @@
 # Calculate Gravity over time, neglecting air resistance
 # Made with <3 by Multipixelone
 # Define default variables, include class
-gravity = 10 # Define gravity in m/s. Real Life would be 9.78033. This assignment can be safely round to 10
+gravity = 1000 # Define gravity in cm/s. Real Life would be 9.78033. This assignment can be safely round to 10
 
 # Ask user some questions
-puts "Please remember, all values are in m/s"
+puts "Please remember, all values are in cm/s"
 puts "What interval (in seconds) should we increase by?"
 interval = gets
-puts "What is the max distance we should calculate to? (In m/s)"
+puts "What is the max distance we should calculate to? (In cm/s)"
 max = gets
 
 # Run some calculations
 halfgrav = (gravity / 2)
-puts halfgrav
+calculations = (max / interval)
+
+puts halfgrav * 1**2
+puts halfgrav * 2**2
+
+# Start loop
+#i = 0
+#while i < calculations
+#  distance = (halfgrav * i)
+#end
